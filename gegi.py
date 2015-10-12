b1cdd2a1a37cffc50245edaa63bdd157fbad8158
ytmytm <mwitkowiak@gmail.com>
2015-10-12 16:24:26 +0200

    implement serial control

--- gegi.py
+++ gegi.py
@@ -1,5 +1,6 @@
 import krpc
 import time
+import serial
 from msvcrt import kbhit,getch
 
 conn = None
@@ -11,6 +12,10 @@ while conn==None:
 		time.sleep(5)
 
 print("Connection successful "+conn.krpc.get_status().version)
+
+ser=serial.Serial("COM7",38400,timeout=1)
+if not ser.isOpen():
+	print("Can't open COM7!")
 
 vessel = None
 while vessel==None:
@@ -29,6 +34,8 @@ flight   = conn.add_stream(vessel.flight,vessel.orbit.body.reference_frame)
 speed	 = conn.add_stream(vessel.flight,vessel.orbit.body.non_rotating_reference_frame)
 orbit	 = vessel.orbit
 
+lastrcs = control.rcs
+
 while True:
 	print("---------CONTROL")
 	print("SAS:"+str(control.sas)+"\tRCS:"+str(control.rcs))
@@ -41,6 +48,33 @@ while True:
 	print("---------FLIGHT")
 	print("Altitude: "+str(round(flight().mean_altitude,0))+"\tSpeed: "+str(round(speed().speed,2)))
 	print("Pitch :"+str(round(flight().pitch,1))+"\tRoll :"+str(round(flight().roll,1))+"\t Head :"+str(round(flight().heading,1)))
+
+	
+	# serial link
+	if ser.isOpen():
+		line = "?"
+		while len(line)>0:
+			line = ser.readline().decode("utf-8").rstrip()
+			print("Serial:["+line+"]\n----\n")
+			if line=="I":
+				lastrcs=None
+			if line=="D0=1":
+				control.activate_next_stage()
+			if line[:3]=="P0=":
+				control.throttle = int(line[3:],16)/255
+
+		# serial state change
+		if control.rcs!=lastrcs:
+			lastrcs = control.rcs
+			if control.rcs:
+				ser.write(b"LG1=1\n")
+				ser.write(b"LR1=0\n")
+			else:
+				ser.write(b"LG1=0\n")
+				ser.write(b"LR1=1\n")
+
+
+	# local console
 	if kbhit():
 		k = ord(getch())
 		if k == 13:
@@ -60,4 +94,4 @@ while True:
 		if (k>48) and (k<=57):
 			control.throttle = (k-48)/10
 			
-	time.sleep(1)
+	#time.sleep(1)

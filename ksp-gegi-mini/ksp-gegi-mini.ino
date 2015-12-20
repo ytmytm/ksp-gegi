b7758f42662b17e406c50b2e96f28dc896b4210a
ytmytm <mwitkowiak@gmail.com>
2015-12-20 23:49:45 +0100

    removed lcd part implemented serial shift register led pins can be driven directly or through shift register

--- ksp-gegi-mini/ksp-gegi-mini.ino
+++ ksp-gegi-mini/ksp-gegi-mini.ino
@@ -1,37 +1,54 @@
 
 #include <Arduino.h>
-#include <Wire.h>
-// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
-#include <LiquidCrystal_I2C.h> // Using version 1.2.1
 
 // dł kabelków 15-20cm
 // diody LED: niebieskie (bez rezystora, krótka nóżka) do GND, białe (z rezystorem, długa nóżka) do pinu
 // potencjometr: niebieski z czarną kreską do GND, biały do +5V, niebieski do pinu A0 / A1
 // przełączniki2: niebieski do GND, biały do pinu, wszystkie zamontowane tak, aby niebieski był w środku, biały na dole, a niepodłączone u góry (wtedy gałka w górę==załączenie)
 // przełączniki3: środkowy (ciemny) do GND, boczne do pinów
-// LCD: SDA->A4, SCL->A5
+
+// 595:
+// 15  A1 SCRCK(13) // clock
+// 16  A2 SERIN(3)  // data
+// 17  A3 RCK(12)   // latch
+// /SRCLR(8) -> +5V
+// /G (9) -> GND
+// /DRAINx = GND od LEDa (LED niebieskie, bez rezystora, krótka nożka do drain; białe, z rezystorem, długa nóżka do +5V)
+
+const uint8_t clockPin = A1;  // SCRCK (13)
+const uint8_t dataPin  = A2;  // SERIN (3)
+const uint8_t latchPin = A3;  // RCK (12)
 
 class digitalPin {
-  public:
+  public: // pinOn/Off - 0-127=hw pin, 128+[0..7]=serial pin
     digitalPin(const uint8_t id, const uint8_t pinSwitch, const uint8_t pinOn, const uint8_t pinOff);
     void updateSwitch();
     void updateLedState(const uint8_t c, const uint8_t val);
-    void updateLed(const bool blink) const ;
+    void updateLed(const bool blink) ;
     const uint8_t getId() const { return(m_id); }
+    const uint8_t getOnOffBits() const; // return object where serial bits are on/off according to state m_OnLed+m_Offled
   private:
     const uint8_t m_id, m_pinSwitch, m_pinOn, m_pinOff;
+    uint8_t m_OnLed { LOW };
+    uint8_t m_OffLed { LOW };
     uint8_t m_lastPinState { HIGH } ;
     uint8_t m_lastLedState { 0 } ; // bits: 0=GxON, 1=RxON, 2=GxBLINK, 3=RxBLINK
 };
 
 digitalPin digiPins[] = {
-  digitalPin(0,4,0,0),    // stage (id, pin switch, pin green LED (on), pin red LED (off)
-  digitalPin(1,5,13,0),   // RCS
-  digitalPin(2,8,9,0),    // SAS
-  digitalPin(3,12,0,0),   // gear
-  digitalPin(4,0,0,6),    // overheat
-  digitalPin(5,0,0,7),    // low power
-  digitalPin(6,0,0,3)     // low fuel
+  digitalPin(0,2,0,0),    // OLED mode1 switch 2
+  digitalPin(1,3,0,0),    // OLED mode2 switch 3
+  digitalPin(2,4,0,0),    // LCD mode1 switch 4
+  digitalPin(3,5,0,0),    // LCD mode2 switch 5
+  digitalPin(4,6,0+0x80,4+0x80),    // light, switch pin 6, green led 0, red led 4
+  digitalPin(5,7,1+0x80,5+0x80),    // gear, switch pin 7, green led 1, red led 5
+  digitalPin(6,8,2+0x80,6+0x80),    // RCS, switch pin 8, green led 2, red led 6
+  digitalPin(7,9,3+0x80,7+0x80),    // SAS, switch pin 9, green led 3, red led 7
+  digitalPin(8,10,0,0),    // stage, switch pin 10
+  digitalPin(9,11,0,0),    // stagemode, switch pin 11
+  digitalPin(10,0,0,12),    // low fuel, yellow led 12
+  digitalPin(11,0,0,13),    // low fuel, yellow led 13
+  digitalPin(12,0,0,A0),    // low fuel, red led 14=A0
 };
 
 digitalPin::digitalPin(const uint8_t id, const uint8_t pinSwitch, const uint8_t pinOn, const uint8_t pinOff) :
@@ -40,11 +57,11 @@ digitalPin::digitalPin(const uint8_t id, const uint8_t pinSwitch, const uint8_t 
   if (m_pinSwitch>0) {
     pinMode(m_pinSwitch,INPUT_PULLUP);
   }
-  if (m_pinOn>0) {
+  if (m_pinOn>0 && m_pinOn<128) {
     pinMode(m_pinOn,OUTPUT);
     digitalWrite(m_pinOn,LOW);
   }
-  if (m_pinOff>0) {
+  if (m_pinOff>0 && m_pinOn<128) {
     pinMode(m_pinOff,OUTPUT);
     digitalWrite(m_pinOff,LOW);
   }
@@ -79,101 +96,59 @@ void digitalPin::updateLedState(uint8_t c, uint8_t val) {
   m_lastLedState = nval;
 }
 
-void digitalPin::updateLed(const bool blink) const {
+void digitalPin::updateLed(const bool blink) {
   uint8_t nstate;
-  if (m_pinOn>0) {
-    nstate = (m_lastLedState & 0x01) ? HIGH : LOW;
-    if (nstate==HIGH && (m_lastLedState & 0x02) && blink) {
-      nstate = !digitalRead(m_pinOn);
-    }
-//Serial.print("L"); Serial.print(gPin); Serial.print(nstate);
-    digitalWrite(m_pinOn,nstate);
+  nstate = (m_lastLedState & 0x01) ? HIGH : LOW;
+  if (nstate==HIGH && (m_lastLedState & 0x02) && blink) {
+    nstate = !m_OnLed;
   }
-  if (m_pinOff>0) {
-    nstate = (m_lastLedState & 0x10) ? HIGH : LOW;
-    if (nstate==HIGH && (m_lastLedState & 0x20) && blink) {
-      nstate = !digitalRead(m_pinOff);
-    }
-//Serial.print("R"); Serial.print(rPin); Serial.print(nstate);
-    digitalWrite(m_pinOff,nstate);
+  m_OnLed = nstate;
+  if (m_pinOn>0 && m_pinOn<128) {
+    digitalWrite(m_pinOn,m_OnLed);
   }
-}
 
-class analogInPin {
-  public:
-    analogInPin(const uint8_t id, const uint8_t pin, const int threshold);
-    void update();
-  private:
-    const uint8_t m_id, m_pin;
-    const int m_threshold;
-    int m_lastAValue { 0 };
-};
-
-analogInPin analogInPins[] = {
-  analogInPin(0,A0,2),  // throttle
-  analogInPin(1,A1,2)	  // timewarp
-};
-
-analogInPin::analogInPin(const uint8_t id, const uint8_t pin, const int threshold) :
-	m_id(id), m_pin(pin), m_threshold(threshold)
-{
-}
-
-void analogInPin::update() {
-	// map to the range of the analog out
-	int aValue = map(analogRead(m_pin), 0, 1023, 0, 255);
-	// is it different enough from the last reading?
-	if (abs(aValue-m_lastAValue)>m_threshold) {
-		if (aValue<m_threshold) {
-			aValue = 0;
-		}
-		if (aValue>255-m_threshold) {
-			aValue = 255;
-		}
-    // dump status
-	Serial.print("P");
-	Serial.print(m_id);
-	Serial.print("=");
-	Serial.println(aValue,HEX);
-	m_lastAValue = aValue;
+  nstate = (m_lastLedState & 0x10) ? HIGH : LOW;
+  if (nstate==HIGH && (m_lastLedState & 0x20) && blink) {
+    nstate = !m_OffLed;
+  }
+  m_OffLed = nstate;
+  if (m_pinOff>0 && m_pinOff<128) {
+    digitalWrite(m_pinOff,m_OffLed);
   }
 }
 
-class analogOutPin {
-  public:
-    analogOutPin(const uint8_t id, const uint8_t pin);
-    void updateState(const uint8_t val) const ;
-    const uint8_t getId() const { return(m_id); }
-  private:
-    const uint8_t m_id, m_pin;
-};
-
-analogOutPin::analogOutPin(const uint8_t id, const uint8_t pin) :
-	m_id(id), m_pin(pin)
-{
+const uint8_t digitalPin::getOnOffBits() const {
+  uint8_t nstate = 0;
+  if (m_pinOn>=128 && m_OnLed) {
+    nstate |= 1 << (m_pinOn & 0x07);
+  }
+  if (m_pinOff>=128 && m_OffLed) {
+    nstate |= 1 << (m_pinOff & 0x07);
+  }
+  return(nstate);
 }
-
-void analogOutPin::updateState(const uint8_t val) const {
-	analogWrite(m_pin,val);
-}
-
-analogOutPin analogOutPins[] = {
-  analogOutPin(0,10),	// g-force
-  analogOutPin(0,11)  // electrical power
-};
 
 // blinker control
 unsigned long lastBlink = 0;
 const unsigned long blinkInterval = 500;
 
-// Set the pins on the I2C chip used for LCD connections:
-//                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
-LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+void serialShiftWrite(uint8_t data) {
+  digitalWrite(latchPin, LOW);
+  delayMicroseconds(100);
+  // shift the bytes out:
+  shiftOut(dataPin, clockPin, MSBFIRST, data);
+  delayMicroseconds(100);
+  digitalWrite(latchPin, HIGH);
+}
 
 // serial port
 #define SERIAL_SPEED 115200
 
 void setup() {
+  //set pins to output so you can control the shift register
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
   // initialize serial communications at 9600 bps:
   Serial.begin(SERIAL_SPEED);
   // set timeout to 200ms
@@ -182,22 +157,12 @@ void setup() {
   Serial.println("I");
   // start blink counter
   lastBlink = millis();
-  lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
-  lcd.setCursor(0,0);
-  lcd.print("KSP-Gegi");
-  lcd.setCursor(0,1);
-  lcd.print("Ready for action");
-}
-
-void updateAnalogs() {
-  for (auto &p : analogInPins) {
-    p.update();
-  }
 }
 
 void updatePins() {
   const unsigned long currentMillis = millis();
   bool blink = false;
+  uint8_t shiftLeds = 0;
   // is it time for tick?
   if (((unsigned long)(currentMillis - lastBlink) >= blinkInterval)) {
     blink = true;
@@ -206,7 +171,9 @@ void updatePins() {
   for (auto &p : digiPins) {
     p.updateLed(blink);
     p.updateSwitch();
+    shiftLeds |= p.getOnOffBits();
   }
+  serialShiftWrite(shiftLeds);
 }
 
 // commands like: ^L{GR}{id}={val}$
@@ -223,56 +190,11 @@ void handleSerialInputLed() {
     }
 }
 
-// commands like ^A{id}={val}$
-void handleSerialInputAnalogOut() {
-    const uint8_t id = Serial.parseInt();  // skip '='
-    const uint8_t val = Serial.parseInt(); // skip newline
-    for (auto &p : analogOutPins) {
-		  if (p.getId()==id) {
-			  p.updateState(val);
-		  }
-	  }
-}
-
-// commands like ^P{line:0,1}={text0..15}$ (consume all characters until end of the line)
-void handleSerialInputLCD() {
-    uint8_t i=0, c;
-    const uint8_t id = Serial.parseInt();
-    Serial.read(); // skip '='
-//  Serial.print("LCDin"); Serial.println(id);
-	  if (id==0 || id==1) {
-		  lcd.setCursor(0,id);
-		  c = -1;
-		  while (c!='\r' && c!='\n') {
-			  if (Serial.available()>0) {
-				  c = Serial.read();
-			  } else {
-				  c = -1;
-			  }
-			  if (c>0 && c!='\r' && c!='\n' && i<16) {
-          i++;
-				  lcd.write(c);
-          //Serial.print(c,HEX);
-			  }
-		  }
-      for (;i<16;i++) {
-        lcd.write(' ');
-      }
-//    Serial.println("endLCD");
-	}
-}
-
 void checkSerialInput() {
   if (Serial.available()>0) {
     const uint8_t c = Serial.read();
     if (c == 'L') {
 	    handleSerialInputLed();
-	  }
-    if (c == 'A') {
-	    handleSerialInputAnalogOut();
-	  }
-    if (c == 'P') {
-	    handleSerialInputLCD();
 	  }
   }
 }
@@ -280,7 +202,6 @@ void checkSerialInput() {
 void loop() {
   checkSerialInput();
   updatePins();
-  updateAnalogs();
   delay(100);
 }
 
